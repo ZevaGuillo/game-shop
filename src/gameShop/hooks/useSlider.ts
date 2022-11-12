a380e24dc212bf77fb56a7f1f7fb1af7cd5882dd
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-12 02:47:37 -0500

    create useSlider hook

---
+++ src/gameShop/hooks/useSlider.ts
@@ -0,0 +1,32 @@
+import { FeaturedType } from '@/types/featuredType';
+import { useState } from 'react'
+
+export const useSlider = (images:FeaturedType[] | string[]) => {
+    const [index, setIndex] = useState(0);
+    const [direction, setDirection] = useState(0);
+  
+    const prevStep = () => {
+      setDirection(1);
+      if (index === 0) {
+        setIndex(images.length - 1);
+        return;
+      }
+      setIndex(index - 1);
+    };
+  
+    const nextStep = () => {
+      setDirection(-1);
+      if (index === images.length - 1) {
+        setIndex(0);
+        return;
+      }
+      setIndex(index + 1);
+    };
+
+    return {
+        index,
+        direction,
+        prevStep,
+        nextStep
+    }
+}
