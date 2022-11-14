802cbbdf238fd836fe662ce31acc6ae7f376e0dd
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-13 23:51:45 -0500

    implement query for gender

--- src/gameShop/pages/GameShopPage.tsx
+++ src/gameShop/pages/GameShopPage.tsx
@@ -1,22 +1,37 @@
 import { getFeatured } from '@/lib/queries/featured';
 import styled from 'styled-components';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueries } from '@tanstack/react-query';
 import Slidershow from '../components/Slidershow';
 import { FeaturedType } from '@/types/featuredType';
+import { getGender } from '@/lib/queries/gender';
 
 
 const GameShopPage = () => {
 
-  const { isLoading , error, data:featured } = useQuery({ queryKey: ['featured'], queryFn: getFeatured })
+  const [featured, gender] = useQueries({
+    queries: [
+      {
+        queryKey: ['featured'],
+        queryFn: getFeatured,
+      },
+      {
+        queryKey: ['gender'],
+        queryFn: getGender,
+      }
+    ]
+  })
 
-  if(isLoading) return<></>;
+  if(featured.isLoading || gender.isLoading) return<></>;
 
-  if(error) return<h2>Error</h2>;
+  if(featured.error || gender.error) return<h2>Error</h2>;
+
+  console.log(gender.data);
+  
 
   return (
     <StyledGameShop className='container'>
       <section>
-        <Slidershow featured={featured as FeaturedType[]}/>
+        <Slidershow featured={featured.data as FeaturedType[]}/>
       </section>
     </StyledGameShop>
   )
---
+++ src/lib/queries/gender.ts
@@ -0,0 +1,13 @@
+import { GenderType } from "@/types/genderType";
+import client from "../sanity";
+
+const query = `*[_type == "gender"] {
+  _id,
+  name,
+}`;
+
+
+export const getGender = async (): Promise<GenderType[]> =>{
+  const results = await client.fetch(query);
+  return results;
+};
