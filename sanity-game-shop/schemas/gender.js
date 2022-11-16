e37975673982770b29dd0492b0df69fde245d879
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-11-16 00:00:56 -0500

    Edit gender schema

--- sanity-game-shop/schemas/gender.js
+++ sanity-game-shop/schemas/gender.js
@@ -8,6 +8,30 @@ export default {
             title: 'Name',
             type: 'string',
             validation: (rule) => rule.required(),
+        },
+        {
+            name:'slug',
+            title:'Slug',
+            type:'slug',
+            options:{
+                source: 'name'
+            }
+        },
+        {
+            name: 'background',
+            type: 'image',
+            title: 'Background',
+            options: {
+                hotspot: true 
+            },
+        },
+        {
+            name: 'icon',
+            type: 'image',
+            title: 'Icon',
+            options: {
+                hotspot: true 
+            },
         }
     ]
 }
