6296f803d7f049f107c778dc9ccf1dd2e3060486
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-02-26 17:54:04 -0500

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { useAppSelector, useAppDispatch } from './hooks/redux';
-import { useEffect, useState } from 'react';
-import { startSanityHome } from './store/gameShop/thunks';
 import AppRouter from './router/AppRouter';
 import { ThemeProvider } from 'styled-components';
-import { Normalize } from 'styled-normalize'
 import { GlobalStyle } from './theme/GlobalStyle';
-import { darkTheme, StyleTheme, theme } from './theme/Theme';
-import { setDarkMode } from './store/gameShop/gameShopSlice';
+import { darkTheme, theme } from './theme/Theme';
 
 
 function App() {
